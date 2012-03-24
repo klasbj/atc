b6a1fd9b2e8fe02f3ac945f4555f26dd44b140b0
Klas Björkqvist <klas.bjorkqvist@gmail.com>
2012-03-24 17:43:08 +0100

    Added target altitude, changed displayed strings

--- js/atc.js
+++ js/atc.js
@@ -9,6 +9,8 @@ function plane(_id,_x,_y,_alt,_dir,_speed) {
     this.dir = _dir;
     this.speed = _speed;
     this.alt = _alt;
+    this.target_alt = 10000;
+    this.fpm = 3000;
 
     /* dt is time difference in ms */
     this.updatepos = function(dt) {
@@ -18,10 +20,29 @@ function plane(_id,_x,_y,_alt,_dir,_speed) {
             * 50.;                  // 1/10 nm since last
         this.x = this.x + dlen * Math.cos(d);
         this.y = this.y + dlen * Math.sin(d);
+
+        dalt = this.target_alt - this.alt;
+        if (Math.abs(dalt) > .000001) {
+            sdalt = dalt / Math.abs(dalt);
+            this.alt = this.alt + sdalt * Math.min(sdalt*dalt, (this.fpm/60.) * (dt/1000.) );
+        }
     }
 
     this.draw_dir = function() {
         return (this.dir + 270) * Math.PI / 180;
+    }
+
+    this.alt_string = function() {
+        str = "" + Math.round(this.target_alt/100);
+        if (this.target_alt > this.alt) {
+            str = str + "+";
+        } else if (this.target_alt < this.alt) {
+            str = str + "-";
+        } else {
+            str = str + "=";
+        }
+        str = str + Math.round(this.alt/100);
+        return str;
     }
 }
 
@@ -34,10 +55,10 @@ function handleKeyUp(evt) {
             planes[0].dir = (planes[0].dir + 10) % 360;
             break;
         case 40:    // down
-            planes[0].speed = Math.max(0,planes[0].speed-10);
+            planes[0].target_alt = Math.max(0,planes[0].target_alt-1000);
             break;
         case 38:    // up
-            planes[0].speed = Math.max(0,planes[0].speed+10);
+            planes[0].target_alt = planes[0].target_alt+1000;
             break;
 
         case 'n'.charCodeAt(0):
@@ -99,7 +120,7 @@ function drawplane(p) {
     context.stroke();
 
 
-    txt = p.speed + " =" + Math.round(p.alt / 100);
+    txt = p.alt_string();
 
     mobj = context.measureText(txt).width;
 
@@ -111,7 +132,8 @@ function drawplane(p) {
 
 
     context.fillStyle="#00ff00";
-    context.fillText(p.id, tx, ty);
-    context.fillText(txt, tx, ty+11);
+    context.fillText(p.id, tx, ty-11);
+    context.fillText(txt, tx, ty);
+    context.fillText(""+p.speed, tx, ty+11);
 
 }
